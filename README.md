2201733851e0c4c28625023c0f59e769584140f7
Emre Saglam <emresaglam@gmail.com>
2021-05-04 16:37:03 -0700

    Cleaned up the code a bit. python3 fixes.

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 # Shodan Bulk IP Query
 This tool gets a list of IPs in a file (one IP per line) and queries shodan.io. It prints the query results in JSON format to stdout. 
 
+# Prerequisite
+* `pip install -r requirements.txt`
+* Define your SHODAN_API_KEY in a file called `.env` in the root directory of the app.
+* Get your IPs in a file \n separated. See `iplist.txt` file for an example. 
+
 # Usage
 `# python ipQuery.py` will take as default filename: `iplist.txt` as filename to read the IPs from. You can always use `-f` or `--filename` as an argument to provide your filename. 
 
-`# python -f myawesomepath/myawesomefilename.foo`
+`# python ipQuery.py -f myawesomepath/myawesomefilename.foo`
+
 
 # TODO
 - Throttling for big queries.
--- ipQuery.py
+++ ipQuery.py
@@ -1,23 +1,27 @@
 import json
 import shodan
 import argparse
-SHODAN_API_KEY = "<YOUR SHODAN API KEY HERE>"
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+SHODAN_API_KEY = os.getenv("SHODAN_API_KEY")
 api = shodan.Shodan(SHODAN_API_KEY)
 
 parser = argparse.ArgumentParser(description='Get a list of IPs and return shodan info.')
 parser.add_argument('--filename', '-f', default='iplist.txt')
-args=parser.parse_args()
+argsc = parser.parse_args()
 
-with open(args.filename, 'r') as f:
-	ips = [line.strip() for line in f]
+with open(argsc.filename, 'r') as f:
+    ips = [line.strip() for line in f]
 
-
-awsInfo = {}
+ipInfo = {}
 for ip in ips:
-	try:
-		hostinfo = api.host(ip)
-		awsInfo[ip] = hostinfo
-	except shodan.APIError, e:
-		awsInfo[ip] = '{}'.format(e)
+    try:
+        hostinfo = api.host(ip)
+        ipInfo[ip] = hostinfo
+    except shodan.APIError as e:
+        ipInfo[ip] = '{}'.format(e)
 
-print json.dumps(awsInfo)
+print(json.dumps(ipInfo))

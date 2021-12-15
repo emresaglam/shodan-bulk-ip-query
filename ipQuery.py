c43b7fcaa6d281f2adfb6604906bc002e4b949e9
Eli McRae <shyft@shyft.us>
2021-12-15 11:09:51 -0600

    Update ipQuery.py
    added delay argument and delay
    added output so I can see what's up

--- ipQuery.py
+++ ipQuery.py
@@ -14,6 +14,8 @@ api = shodan.Shodan(SHODAN_API_KEY)
 
 parser = argparse.ArgumentParser(description='Get a list of IPs and return shodan info.')
 parser.add_argument('--filename', '-f', default='iplist.txt')
+parser.add_argument('--delay', '-d', default=0)
+parser.add_argument('--verbose', '-v', default=True)
 argsc = parser.parse_args()
 
 with open(argsc.filename, 'r') as f:
@@ -21,11 +23,15 @@ with open(argsc.filename, 'r') as f:
 
 ipInfo = {}
 for ip in ips:
+    if argsc.verbose:
+        print(f'[INFO] - fetching host {ip}') 
     try:
         hostinfo = api.host(ip)
         ipInfo[ip] = hostinfo
-        time.sleep(1)
+        time.sleep(argsc.delay)
     except shodan.APIError as e:
+        if argsc.verbose:
+            print(f'[ERROR] - fetching host {ip}', e)
         ipInfo[ip] = '{}'.format(e)
 
 print(json.dumps(ipInfo))
